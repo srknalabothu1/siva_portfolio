c98e724d1043f881e46d3aa2d7d009551f1df749
Siva <sivaramakrishna.nalabothu@myabank.com>
2024-06-14 12:38:00 +0800

    new chamnges

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -25,8 +25,8 @@ const About = () => {
             </div>
             <div className="m-6">
                 <h1 className='text-2xl text-2rem font-serif'><b>Hello there!! 👋 </b></h1> <br />
-                <h2 className='text-1.75em font-serif'>I'am Siva Ramakrishna, nice to meet you. Please take a look around!</h2><br />
-                <h5 className='font-serif text-base'>I am passionate about building excellent software that improves the lives of those around me. I specialize in building applications specific to the business needs of my clients. I have done work in software development, mobile app creation, front-end/back-end web, database/server management development. I have 6+ years of experience of building and maintaining Websites, Mobile Applications.I love Javascript, Node, React, Redux, Html, CSS, Bootstrap.</h5>
+                <h2 className='text-1.75em font-serif'>I'm Siva Ramakrishna, nice to meet you. Please take a look around!</h2><br />
+                <h5 className='font-serif text-base'>I am passionate about building excellent software that improves the lives of those around me. I specialize in building applications specific to the business needs of my clients. I am experienced in software development, mobile app creation, front-end/back-end web, database/server management development. I have around 6+ years of experience of building and maintaining Websites, Mobile Applications.I love Javascript, Node, React, Redux, Html, CSS, Bootstrap.</h5>
                 <div className='font-serif pt-[5rem] pl-[33rem]'><button className='p-2.5 leading-4 m-4 border-2 border-white rounded-3xl' onClick={onResumeOpen}>View resume</button></div>
             </div>
         </div>
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -21,17 +21,19 @@ const Experience = () => {
     return (
         <div className="text-black p-10 ml-5 text-xl">
             <h2 className="text-2xl font-bold text-gray-900">EXPERIENCE <span className="text-red-500">SUMMARY</span></h2>
-            <div className="space-y-6 mt-8">
-                {experiences.map((exp, index) => <div className="flex space-x-4 items-start" key={index}>
-                    <div className="flex-shrink-0 w-8 h-8 bg-blue-500 text-white rounded-full flex items-center justify-center">
-                        {index + 1}
-                    </div>
-                    <div>
-                        <h3 className="text-xl font-semibold text-gray-900">{exp.position}</h3>
-                        <p className="text-gray-700">{exp.company}</p>
-                        <p className="text-gray-500">{exp.duration}</p>
-                    </div>
-                </div>)}
+            <div  style={{ borderLeft: "dotted", borderRight: "none", borderBottom: "none", borderTop: "none" }} >
+                <div className="space-y-6 mt-8" style={{ marginLeft: "-17px" }}>
+                    {experiences.map((exp, index) => <div className="flex space-x-4 items-start" key={index}>
+                        <div className="flex-shrink-0 w-8 h-8 bg-blue-500 text-white rounded-full flex items-center justify-center">
+                            {index + 1}
+                        </div>
+                        <div>
+                            <h3 className="text-xl font-semibold text-gray-900">{exp.position}</h3>
+                            <p className="text-gray-700">{exp.company}</p>
+                            <p className="text-gray-500">{exp.duration}</p>
+                        </div>
+                    </div>)}
+                </div>
             </div>
         </div>
     );
